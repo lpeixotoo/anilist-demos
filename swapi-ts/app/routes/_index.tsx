d87b2ec4d72c90e54d50d3d5c941ecde70338ecf
lpeixotoo <lucasapeixoto@gmail.com>
2025-03-25 18:16:17 -0300

    feat(anilist-ts): Add shadcn + table

--- swapi-ts/app/routes/_index.tsx
+++ swapi-ts/app/routes/_index.tsx
@@ -1,4 +1,13 @@
 import type { MetaFunction } from "@remix-run/node";
+import {
+	Table,
+	TableBody,
+	TableCaption,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "~/components/ui/table";
 
 export const meta: MetaFunction = () => {
 	return [
@@ -9,64 +18,24 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
 	return (
-		<div className="flex h-screen items-center justify-center">
-			<div className="flex flex-col items-center gap-16">
-				<header className="flex flex-col items-center gap-9">
-					<h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-						Welcome to <span className="sr-only">Remix</span>
-					</h1>
-					<div className="h-[144px] w-[434px]">
-						<img
-							src="/logo-light.png"
-							alt="Remix"
-							className="block w-full dark:hidden"
-						/>
-						<img
-							src="/logo-dark.png"
-							alt="Remix"
-							className="hidden w-full dark:block"
-						/>
-					</div>
-				</header>
-				<nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
-					<p className="leading-6 text-gray-700 dark:text-gray-200">
-						What&apos;s next?
-					</p>
-					<ul>
-						{resources.map(({ href, text }) => (
-							<li key={href}>
-								<a
-									className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-									href={href}
-									target="_blank"
-									rel="noreferrer"
-								>
-									{text}
-								</a>
-							</li>
-						))}
-					</ul>
-				</nav>
-			</div>
-		</div>
+		<Table>
+			<TableCaption>A list of your recent invoices.</TableCaption>
+			<TableHeader>
+				<TableRow>
+					<TableHead className="w-[100px]">Invoice</TableHead>
+					<TableHead>Status</TableHead>
+					<TableHead>Method</TableHead>
+					<TableHead className="text-right">Amount</TableHead>
+				</TableRow>
+			</TableHeader>
+			<TableBody>
+				<TableRow>
+					<TableCell className="font-medium">INV001</TableCell>
+					<TableCell>Paid</TableCell>
+					<TableCell>Credit Card</TableCell>
+					<TableCell className="text-right">$250.00</TableCell>
+				</TableRow>
+			</TableBody>
+		</Table>
 	);
 }
-
-const resources = [
-	{
-		href: "https://remix.run/start/quickstart",
-		text: "Quick Start (5 min)",
-	},
-	{
-		href: "https://remix.run/start/tutorial",
-		text: "Tutorial (30 min)",
-	},
-	{
-		href: "https://remix.run/docs",
-		text: "Remix Docs",
-	},
-	{
-		href: "https://rmx.as/discord",
-		text: "Join Discord",
-	},
-];
