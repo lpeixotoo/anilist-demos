0f501e135de1cc02bed40d4f678c019f05a37921
lpeixotoo <lucasapeixoto@gmail.com>
2025-03-30 21:19:05 -0300

    refact(anilist-ts): Refact carousel to generic behaviour

--- anilist-ts/app/routes/_index.tsx
+++ anilist-ts/app/routes/_index.tsx
@@ -1,5 +1,7 @@
+import { useQuery } from "@apollo/client";
 import type { MetaFunction } from "@remix-run/node";
-import AnimeList from "~/components/ui/AnimeList";
+import { PaginatedAnimeCarousel } from "~/components/ui/PaginatedAnimeCarousel";
+import { GET_SORTED_MEDIA_PAGE } from "~/services/anilist";
 
 export const meta: MetaFunction = () => {
 	return [
@@ -8,6 +10,49 @@ export const meta: MetaFunction = () => {
 	];
 };
 
+const PAGE_SIZE = 10;
+
 export default function Index() {
-	return <AnimeList />;
+	const {
+		loading,
+		error,
+		data: mostPopularAnime,
+		fetchMore,
+	} = useQuery(GET_SORTED_MEDIA_PAGE, {
+		variables: {
+			page: 1,
+			pageSize: 10,
+			sortBy: "POPULARITY_DESC",
+			sortType: "ANIME",
+		},
+		notifyOnNetworkStatusChange: true,
+	});
+
+	const handleFetchMore = (page: number) => {
+		fetchMore({
+			variables: {
+				page,
+			},
+			updateQuery: (prev, { fetchMoreResult }) => {
+				if (!fetchMoreResult) return prev;
+				return {
+					...fetchMoreResult,
+					Page: {
+						...fetchMoreResult.Page,
+						media: [...prev.Page.media, ...fetchMoreResult.Page.media],
+					},
+				};
+			},
+		});
+	};
+
+	return (
+		<PaginatedAnimeCarousel
+			pageSize={PAGE_SIZE}
+			loading={loading}
+			error={error?.message}
+			animeMedias={mostPopularAnime?.Page?.media ?? []}
+			fetchOnScrollEnd={handleFetchMore}
+		/>
+	);
 }
